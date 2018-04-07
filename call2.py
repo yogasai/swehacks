0caacf014afed52771d9b6887bf96c1a696169e9
Satvik Shukla <sshukla9@cs.washington.edu>
2018-04-07 15:57:10 -0700

    call2 mod

--- call2.py
+++ call2.py
@@ -2,7 +2,20 @@ import requests
 import json
 
 response = requests.get('https://api.github.com/repos/code-dot-org/code-dot-org/comments', auth = ('satvikshukla', '&azgithub19'))
-dict2 = json.loads(response.content)[0]
-# print(response.text)
-# print(response.content)
-print(dict2['body'])
+
+# for i in response(1,10):
+# 	pass
+
+i = 0
+str = ""
+
+# for obj in json.loads(response.content)
+# 	print("a")
+	# dict2 = 
+	# print(dict2['body'])
+print(len(json.loads(response.content)))
+
+while (i < 30):
+	dict2 = json.loads(response.content)[i]
+	print(dict2['body'])
+	i = i + 1
